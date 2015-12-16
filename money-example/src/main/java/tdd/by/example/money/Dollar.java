6b8445a433ef3df8385c708e5f7bc9606ec9de6b
Csaba J Santa <santacs@gmail.com>
2015-12-16 06:54:29 +0100

    p5.4

--- money-example/src/main/java/tdd/by/example/money/Dollar.java
+++ money-example/src/main/java/tdd/by/example/money/Dollar.java
@@ -2,13 +2,14 @@ package tdd.by.example.money;
 
 public class Dollar {
 
+    public int amount;
+
     public Dollar(int amount) {
         // TODO Auto-generated constructor stub
     }
 
     public void times(int multiplier) {
         // TODO Auto-generated method stub
-
     }
 
 }
