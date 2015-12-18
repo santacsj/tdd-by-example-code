b2c105670257474883e90a9da704278d15808c65
Csaba J Santa <santacs@gmail.com>
2015-12-18 08:17:19 +0100

    p17.1

--- money-example/src/main/java/tdd/by/example/money/Dollar.java
+++ money-example/src/main/java/tdd/by/example/money/Dollar.java
@@ -14,6 +14,7 @@ public class Dollar {
 
     @Override
     public boolean equals(Object obj) {
-        return true;
+        Dollar dollar = (Dollar) obj;
+        return amount == dollar.amount;
     }
 }
