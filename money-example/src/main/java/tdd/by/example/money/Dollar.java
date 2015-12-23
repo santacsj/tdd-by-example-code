fa93eb49a647c3590a689c383d3edf70b0310508
Csaba J Santa <santacs@gmail.com>
2015-12-23 17:07:07 +0100

    p29.2

--- money-example/src/main/java/tdd/by/example/money/Dollar.java
+++ money-example/src/main/java/tdd/by/example/money/Dollar.java
@@ -12,7 +12,7 @@ public class Dollar extends Money {
 
     @Override
     public boolean equals(Object obj) {
-        Money dollar = (Money) obj;
-        return amount == dollar.amount;
+        Money money = (Money) obj;
+        return amount == money.amount;
     }
 }
