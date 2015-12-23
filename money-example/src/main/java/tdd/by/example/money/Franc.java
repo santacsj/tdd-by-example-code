cfb64a09c944fd8443ec650b424e0d59dd0077dd
Csaba J Santa <santacs@gmail.com>
2015-12-23 17:14:28 +0100

    p31.1

--- money-example/src/main/java/tdd/by/example/money/Franc.java
+++ money-example/src/main/java/tdd/by/example/money/Franc.java
@@ -9,10 +9,4 @@ public class Franc extends Money {
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        Money money = (Money) obj;
-        return amount == money.amount;
-    }
 }
