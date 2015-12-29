9bc7da35c7a033cfef9ef3b492cb1bb255557a08
Csaba J Santa <santacs@gmail.com>
2015-12-29 16:37:14 +0100

    p36.4

--- money-example/src/main/java/tdd/by/example/money/Money.java
+++ money-example/src/main/java/tdd/by/example/money/Money.java
@@ -1,12 +1,14 @@
 package tdd.by.example.money;
 
-public class Money {
+public abstract class Money {
 
     static Dollar dollar(int amount) {
         return new Dollar(amount);
     }
 
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object obj) {
