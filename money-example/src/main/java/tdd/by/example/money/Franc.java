02f8791f177cb8d1c15034dc67b93e40270b5c0b
Csaba J Santa <santacs@gmail.com>
2015-12-29 17:20:06 +0100

    p41.2

--- money-example/src/main/java/tdd/by/example/money/Franc.java
+++ money-example/src/main/java/tdd/by/example/money/Franc.java
@@ -2,12 +2,12 @@ package tdd.by.example.money;
 
 public class Franc extends Money {
 
-    public Franc(int amount) {
+    public Franc(int amount, String currency) {
         this.amount = amount;
         currency = "CHF";
     }
 
     public Money times(int multiplier) {
-        return new Franc(amount * multiplier);
+        return new Franc(amount * multiplier, null);
     }
 }
