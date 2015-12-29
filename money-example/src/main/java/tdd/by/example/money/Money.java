435e63bb30938e86739bd3c4f2d2958e14a73f31
Csaba J Santa <santacs@gmail.com>
2015-12-29 16:37:40 +0100

    p36.5

--- money-example/src/main/java/tdd/by/example/money/Money.java
+++ money-example/src/main/java/tdd/by/example/money/Money.java
@@ -2,7 +2,7 @@ package tdd.by.example.money;
 
 public abstract class Money {
 
-    static Dollar dollar(int amount) {
+    static Money dollar(int amount) {
         return new Dollar(amount);
     }
 
