697ecd488e3de4133620c51ae441b264f649dfa2
Csaba J Santa <santacs@gmail.com>
2015-12-29 21:14:01 +0100

    p52.2

--- money-example/src/test/java/tdd/by/example/money/DollarTest.java
+++ money-example/src/test/java/tdd/by/example/money/DollarTest.java
@@ -17,9 +17,7 @@ public class DollarTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-        assertFalse(Money.dollar(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     @Test
