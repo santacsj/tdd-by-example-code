d7661bbafde0195f641c15713ffa2d434dc5f397
Csaba J Santa <santacs@gmail.com>
2015-12-30 16:54:03 +0100

    p69.4

--- money-example/src/main/java/tdd/by/example/money/Bank.java
+++ money-example/src/main/java/tdd/by/example/money/Bank.java
@@ -1,6 +1,10 @@
 package tdd.by.example.money;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable<Pair, Integer> rates = new Hashtable<Pair, Integer>();
 
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
