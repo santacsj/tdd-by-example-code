24fd043985f32dad638dc3120c224786b99c925a
Csaba J Santa <santacs@gmail.com>
2015-12-30 16:47:14 +0100

    p68.1

--- money-example/src/main/java/tdd/by/example/money/Bank.java
+++ money-example/src/main/java/tdd/by/example/money/Bank.java
@@ -3,7 +3,7 @@ package tdd.by.example.money;
 public class Bank {
 
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String from, String to, int rate) {
