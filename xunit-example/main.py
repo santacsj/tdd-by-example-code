ff247bdc88099363d5701e36214eccf5b5017814
Csaba J Santa <santacs@gmail.com>
2016-01-10 11:51:40 +0100

    p92.4

--- xunit-example/main.py
+++ xunit-example/main.py
@@ -3,6 +3,9 @@ class WasRun:
     def __init__(self, name):
         self.wasRun = None
 
+    def testMethod(self):
+        pass
+
 test = WasRun("testMethod")
 print test.wasRun
 test.testMethod()
