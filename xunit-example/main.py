ed3649dc2d39f823a4c8b0ed6ba74ddd8ccf2cbb
Csaba J Santa <santacs@gmail.com>
2016-01-11 20:46:12 +0100

    p98.1

--- xunit-example/main.py
+++ xunit-example/main.py
@@ -18,4 +18,9 @@ class TestCaseTest(TestCase):
         assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
