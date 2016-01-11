7d9b0de61f4460c6a9274ab13c766a11a3d41450
Csaba J Santa <santacs@gmail.com>
2016-01-11 20:53:46 +0100

    p99.1

--- xunit-example/main.py
+++ xunit-example/main.py
@@ -10,9 +10,9 @@ class TestCase:
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
